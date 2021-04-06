83a9945487001bbe90929e69cc6f4eb2c4dce866
ethan <yguo0606@gmail.com>
2021-04-06 14:29:56 +0800

    updated

--- tests/embedding.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-c = tf.constant([[1, 2, 0]])
-e = tf.keras.layers.Embedding(10, 2, mask_zero=True)
-t = e(c)
-print(t)
-r = tf.keras.layers.AveragePooling1D()(t)
-print(r)
